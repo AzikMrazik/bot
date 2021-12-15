570285622d0e4be97417707614b5ee37d296425b
AzikMrazik <82385429+AzikMrazik@users.noreply.github.com>
2021-12-15 08:21:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
 import telebot
 
 bot = telebot.TeleBot(5097465180:AAFezAkYbiVyBPwL1ujdcYG2PuJh8iYYpzs) 
+
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
