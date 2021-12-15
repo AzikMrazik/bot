3e68a707779b46c90b085fda0c1f97238d4fe6a2
AzikMrazik <82385429+AzikMrazik@users.noreply.github.com>
2021-12-15 09:30:02 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+
+bot = Bot(token="5097465180:AAFezAkYbiVyBPwL1ujdcYG2PuJh8iYYpzs")
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+
+@dp.message_handler(commands="test1")
+async def cmd_test1(message: types.Message):
+    await message.reply("Test 1")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
